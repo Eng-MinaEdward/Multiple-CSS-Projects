7b94f6ef0dadffa6b74291058f981d58beff2ba1
mizo <eng.mina.edward@outlook.com>
2022-08-15 14:23:21 +0200

    The Project Second files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'abdoFlexTask2';
+
+  constructor(private router: Router,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.router.navigate(['first'], {relativeTo: this.route}).then();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FirstComponent} from './first/first.component';
+import {ThirdComponent} from './third/third.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'first', component: FirstComponent},
+  {path: 'third', component: ThirdComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+
+  declarations:
+    [
+      AppComponent,
+      FirstComponent,
+      ThirdComponent,
+    ],
+  imports:
+    [RouterModule.forRoot(appRoutes),
+      BrowserModule
+    ],
+  providers:
+    [],
+  bootstrap:
+    [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
